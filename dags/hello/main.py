1c53360654a7d664a054c28d237d02d769f80f52
Nikhil Suryawanshi <84472147+nik-29@users.noreply.github.com>
2022-06-23 10:48:20 +0530

    Update main.py

--- dags/hello/main.py
+++ dags/hello/main.py
@@ -2,7 +2,38 @@ from datetime import datetime
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
+import os
+import pendulum
+from airflow import DAG
+from airflow.decorators import task
+from airflow.example_dags.libs.helper import print_stuff
+from airflow.settings import AIRFLOW_HOME
 
+from kubernetes.client import models as k8s
+executor_config_sidecar = {
+            "pod_override": k8s.V1Pod(
+                spec=k8s.V1PodSpec(
+                    containers=[
+                        k8s.V1Container(
+                            name="base",
+                            volume_mounts=[k8s.V1VolumeMount(mount_path="/shared/", name="shared-empty-dir")],
+                        ),
+                        k8s.V1Container(
+                            name="sidecar",
+                            image="ubuntu",
+                            args=["echo \"retrieved from mount\" > /shared/test.txt"],
+                            command=["bash", "-cx"],
+                            volume_mounts=[k8s.V1VolumeMount(mount_path="/shared/", name="shared-empty-dir")],
+                        ),
+                    ],
+                    volumes=[
+                        k8s.V1Volume(name="shared-empty-dir", empty_dir=k8s.V1EmptyDirVolumeSource()),
+                    ],
+                )
+            ),
+        }
+
+@task(executor_config=executor_config_sidecar)
 def print_hello():
  return 'Hello Wolrd'
 
