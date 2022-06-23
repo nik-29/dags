8422168177581c323119945908057fe8574f4e17
Nikhil Suryawanshi <84472147+nik-29@users.noreply.github.com>
2022-06-23 11:12:38 +0530

    Update main.py

--- dags/hello/main.py
+++ dags/hello/main.py
@@ -2,61 +2,14 @@ from datetime import datetime
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
-import os
-import pendulum
-from airflow import DAG
-from airflow.decorators import task
-from airflow.example_dags.libs.helper import print_stuff
-from airflow.settings import AIRFLOW_HOME
 
-from kubernetes.client import models as k8s
-executor_config_sidecar = {
-            "pod_override": k8s.V1Pod(
-                spec=k8s.V1PodSpec(
-                    containers=[
-                        k8s.V1Container(
-                            name="base",
-                            volume_mounts=[k8s.V1VolumeMount(mount_path="/shared/", name="shared-empty-dir")],
-                        ),
-                        k8s.V1Container(
-                            name="sidecar",
-                            image="ubuntu",
-                            args=["echo \"retrieved from mount\" > /shared/test.txt"],
-                            command=["bash", "-cx"],
-                            volume_mounts=[k8s.V1VolumeMount(mount_path="/shared/", name="shared-empty-dir")],
-                        ),
-                    ],
-                    volumes=[
-                        k8s.V1Volume(name="shared-empty-dir", empty_dir=k8s.V1EmptyDirVolumeSource()),
-                    ],
-                )
-            ),
-        }
+def print_hello():
+ return 'Hello Wolrd'
 
-@task(executor_config=executor_config_sidecar)
-def test_sharedvolume_mount():
-            """
-            Tests whether the volume has been mounted.
-            """
-            for i in range(5):
-                try:
-                    return_code = os.system("cat /shared/test.txt")
-                    if return_code != 0:
-                        raise ValueError(f"Error when checking volume mount. Return code {return_code}")
-                except ValueError as e:
-                    if i > 4:
-                        raise e
-                        
-sidecar_task = test_sharedvolume_mount()
+dag = DAG('hello_world', description='Hello world example', schedule_interval='0 12 * * *', start_date=datetime(2017, 3, 20), catchup=False)
 
+dummy_operator = DummyOperator(task_id='dummy_task', retries = 3, dag=dag)
 
-# def print_hello():
-#  return 'Hello Wolrd'
+hello_operator = PythonOperator(task_id='hello_task', python_callable=print_hello, dag=dag)
 
-# dag = DAG('hello_world', description='Hello world example', schedule_interval='0 12 * * *', start_date=datetime(2017, 3, 20), catchup=False)
-
-# dummy_operator = DummyOperator(task_id='dummy_task', retries = 3, dag=dag)
-
-# hello_operator = PythonOperator(task_id='hello_task', python_callable=print_hello, dag=dag)
-
-# dummy_operator >> hello_operator
+dummy_operator >> hello_operator
